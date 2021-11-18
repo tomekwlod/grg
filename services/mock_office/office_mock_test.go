032835e932b361b03c16dac7b1f1c9badd2edcd0
Tomasz Wlodarczyk <tomek.wlod@gmail.com>
2021-11-18 16:10:12 +0000

    New Office mock

--- services/mock_office/office_mock_test.go
+++ services/mock_office/office_mock_test.go
@@ -2,11 +2,13 @@ package mock_pb_test
 
 import (
 	"context"
+	"fmt"
 	"testing"
 
 	"github.com/golang/mock/gomock"
 	"github.com/tomekwlod/grg/pb"
 	officesmock "github.com/tomekwlod/grg/services/mock_office"
+	"google.golang.org/protobuf/proto"
 )
 
 var noContext = context.Background()
@@ -33,6 +35,38 @@ func TestOfficeService(t *testing.T) {
 
 		testResults(t, mockOfficeClient)
 	})
+
+	t.Run("Office's create test", func(t *testing.T) {
+
+		ctrl := gomock.NewController(t)
+		defer ctrl.Finish()
+
+		req := &pb.CreateOfficeRequest{
+			Name: "Office 1",
+		}
+
+		mockOfficeClient := officesmock.NewMockOfficeServiceClient(ctrl)
+		mockOfficeClient.EXPECT().Create(noContext, &rpcMsg{msg: req}).Return(&pb.CreateOfficeResponse{
+			Id:   1,
+			Name: "Office Mocked",
+		}, nil)
+
+		testCreate(t, mockOfficeClient)
+	})
+}
+
+func testCreate(t *testing.T, client pb.OfficeServiceClient) {
+
+	o, err := client.Create(noContext, &pb.CreateOfficeRequest{Name: "Office 1"})
+
+	if err != nil {
+		t.Errorf("mocking failed, %s", err.Error())
+	}
+
+	if o.Name != "Office Mocked" {
+		t.Errorf("mocking failed, expected %s, got %s", "Office Mocked", o.Name)
+	}
+
 }
 
 func testResults(t *testing.T, client pb.OfficeServiceClient) {
@@ -53,7 +87,21 @@ func testResults(t *testing.T, client pb.OfficeServiceClient) {
 	if o.Results[1].Name != "Office 2" {
 		t.Errorf("mocking failed, expected office 2 to be with name `%s`, got `%s`", "Office 2", o.Results[1].Name)
 	}
+}
 
-	t.Log("ok")
+// rpcMsg implements the gomock.Matcher interface
+type rpcMsg struct {
+	msg proto.Message
+}
 
+func (r *rpcMsg) Matches(msg interface{}) bool {
+	m, ok := msg.(proto.Message)
+	if !ok {
+		return false
+	}
+	return proto.Equal(m, r.msg)
+}
+
+func (r *rpcMsg) String() string {
+	return fmt.Sprintf("is %s", r.msg)
 }
